5b2e241d673be826d3274f21a29da66c757481b7
guoxiaomi <xiaomi.guo@outlook.com>
2023-04-22 19:01:50 +0800

    ~

--- src/base/detail.hpp
+++ src/base/detail.hpp
@@ -184,7 +184,7 @@ struct detail_ext : detail {
   /// @param object 目标 ruby 对象的 VALUE
   /// @return VALUE 对象的实例变量对应的 VALUE
   template <word w>
-  static VALUE get(VALUE value_) {
+  static VALUE get(const VALUE value_) {
     Check_Type(value_, T_OBJECT);
 
     return rb_ivar_get(value_, id_table[static_cast<size_t>(w)]);
@@ -197,7 +197,7 @@ struct detail_ext : detail {
   /// @return T 返回实例变量相应类型的值或 object_id（若目标类型为 const
   /// uint64_t）
   template <word w, typename T>
-  static T get(VALUE value_) {
+  static T get(const VALUE value_) {
     return from_ruby<T>(get<w>(value_));
   }
 };
