96a50619e8d443c0ba086c704634bf5f9d28011c
guoxiaomi <xiaomi.guo@outlook.com>
2023-05-01 11:30:19 +0800

    再测测Pmr

--- src/rmxp/drawable.hpp
+++ src/rmxp/drawable.hpp
@@ -249,13 +249,35 @@ viewport default_viewport;
 /// @name data
 /// Drawables 是有序的，索引是 z_index
 struct drawables {
+#if 1
+  /// @brief 作为内存池的 array
+  /// 所有的 drawables 共享 1M 资源。
+  inline static std::array<char, 1024 * 1024> array_buffer;
+
+  /// @brief 封装 array_buffer 为上游资源池
+  /// 所有的 drawables 共享此资源池。
+  inline static std::pmr::monotonic_buffer_resource buffer_resource{
+      array_buffer.data(), array_buffer.size()};
+
+  /// @brief 线程不安全的资源池，其上游是 buffer_resource
+  /// 所有的 drawables 共享此资源池。
+  inline static std::pmr::unsynchronized_pool_resource pool_resource{
+      std::pmr::pool_options(16, 256), &buffer_resource};
+
+  /// @brief 存储 drawable 的 map
+  /// 所有的 drawable 按照 z_index 有序排列。
+  std::pmr::map<z_index, drawable> m_data;
+
+  /// @brief 构造函数，绑定 map 到资源池
+  explicit drawables() : m_data(&pool_resource) {}
+#else
   /// @brief 存储 drawable 的 map
   /// 所有的 drawable 按照 z_index 有序排列。
   std::map<z_index, drawable> m_data;
 
-  /// @brief 构造函数，初始化资源池和 map
+  /// @brief 构造函数
   explicit drawables() : m_data() {}
-
+#endif
   /// @brief 设置某个 drawable 的新 z 值。
   /// 需要从 map 中取出再放回。
   /// 这个函数不会操作 drawable 的堆上数据，故指针不会变化。
